fb487ff0656b68b37e22077f70804f826bce393b
marcsze <marcasze@gmail.com>
2017-09-18 15:12:05 -0400

    set up conversion equation program

---
+++ code/run_convert_scfa_concentrations.R
@@ -0,0 +1,27 @@
+### Creation of function to make the conversions
+### Need to adjust readings to get the final SCFA concentrations
+### Marc Sze
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# TO DO LIST
+### Create three different linear regressions
+    ### One for each standard (x = concentration, y = Area)
+### Convert each standard based on unique model obtained
+### Create correction based on location run
+### Correct concentration based on location of sample relative to three different standards used
+    ### e.g. use std 1 and 2 if samples between them and std 2 and 3 if samples between them (mM)
+### Convert to mmol/kg based on weight measured and total volume originally suspended in
