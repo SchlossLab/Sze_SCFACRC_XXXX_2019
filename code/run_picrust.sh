c941f09efa14ef5bfea23e3b7b784d1eab275d35
marcsze <marcasze@gmail.com>
2017-10-17 10:21:47 -0400

    change to biom format read out

--- code/run_picrust.sh
+++ code/run_picrust.sh
@@ -17,10 +17,10 @@ biom convert --table-type="OTU table" -i $WORKDIR/gg_final.0.03.biom -o $WORKDIR
 biom convert -i $WORKDIR/temp_OTU_table.txt -o $WORKDIR/gg_corr_OTU_table.biom --table-type="OTU table" --to-json --process-obs-metadata taxonomy
 # Double check that it is now the right format
 biom validate-table -i $WORKDIR/gg_corr_OTU_table.biom
-# Run the picrust program to make predictions
+# Run the picrust program to make predictions 
 $PICRUST/normalize_by_copy_number.py -i $WORKDIR/gg_corr_OTU_table.biom -o $WORKDIR/gg_corr_normalized_otus.biom
-$PICRUST/predict_metagenomes.py -f -i $WORKDIR/gg_corr_normalized_otus.biom -o $WORKDIR/predicted_metagenomes.txt -a $WORKDIR/nsti.txt
-
+$PICRUST/predict_metagenomes.py -i $WORKDIR/gg_corr_normalized_otus.biom -o $WORKDIR/predicted_metagenomes.biom -a $WORKDIR/nsti.txt
+		# using -f tag creates a tab delimited table
 
 #biom convert --table-type="OTU table" -i normalized_otus.biom -o normalized_otus_table.txt --header-key taxonomy --to-tsv
 #biom convert -i normalized_otus.biom -o normalized_otus_table.txt  --table-type "otu table"
