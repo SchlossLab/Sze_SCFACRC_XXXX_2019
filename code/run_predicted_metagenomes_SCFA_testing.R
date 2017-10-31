0f4d4bc4d849751e237b8e47374f95774247fb35
marcsze <marcasze@gmail.com>
2017-10-31 15:35:36 -0400

    complete with high/low coding

--- code/run_predicted_metagenomes_SCFA_testing.R
+++ code/run_predicted_metagenomes_SCFA_testing.R
@@ -41,16 +41,34 @@ nzv_full_pi_data <- full_pi_data[, -nzv]
 ##############################################################################################
 
 # Function to create data groups to be used in the RF model
-make_rf_data <- function(scfa_name, dataTable, scfa_data, exclude_samples){
+make_rf_data <- function(scfa_name, dataTable, scfa_data, 
+                         exclude_samples, column_of_int){
   
   tempData <- scfa_data[[scfa_name]] %>% 
     filter(!(study_id %in% exclude_samples)) %>% 
-    select(study_id, mmol_kg) %>% 
+    select(study_id, contains(column_of_int)) %>% 
     inner_join(dataTable, by = c("study_id" = "sample_id")) %>% 
     select(-study_id)
   
   return(tempData)
 }
+
+
+# Function to generate the high low groupings for each respective SCFA
+get_high_low <- function(all_data_list, scfa_median_list, scfas){
+  
+  tempList <- sapply(scfas, 
+                     function(x) all_data_list[[x]] %>% 
+                       mutate(high_low = ifelse(mmol_kg <= scfa_median_list[[x]], 
+                                                invisible("low"), invisible("high"))) %>% 
+                       select(study_id, mmol_kg, high_low, everything()), 
+                     simplify = F)
+  
+  return(tempList)
+  
+}
+
+
 
 # Function to make an initial 80/20 split within the data
 eighty_twenty_split <- function(i, dataList){
@@ -206,59 +224,73 @@ add_model_summary_data <- function(i, model_list, test_data, data_Table){
 ##############################################################################################
 
 # Set up initial store for summary data
-model_important_vars <- list(adenoma = data_frame(), cancer = data_frame())
+class_important_vars <- list(acetate = data_frame(), butyrate = data_frame(), 
+                             isobutyrate = data_frame(), propionate = data_frame())
 
-model_summary_data <- list(adenoma = data_frame(), cancer = data_frame())
+class_summary_data <- list(acetate = data_frame(), butyrate = data_frame(), 
+                           isobutyrate = data_frame(), propionate = data_frame())
+
+regress_important_vars <- list(acetate = data_frame(), butyrate = data_frame(), 
+                               isobutyrate = data_frame(), propionate = data_frame())
+
+regress_summary_data <- list(acetate = data_frame(), butyrate = data_frame(), 
+                             isobutyrate = data_frame(), propionate = data_frame())
 
 # Set up model type variables
 scfas <- c("acetate", "butyrate", "isobutyrate", "propionate")
+
+# Get scfas medians
+scfa_median_vector <- sapply(scfas, function(x) median(scfa_data[[x]]$mmol_kg))
+
+# Generate High/Low variable vector
+all_scfa_data <- get_high_low(scfa_data, scfa_median_vector, scfas)
+
 # Create RF data tables to be used
-combined_data <- sapply(scfas, 
-                  function(x) make_rf_data(x, nzv_full_pi_data, scfa_data, 
-                                           c(metaF$initial, metaF$followUp)), simplify = F)
-# Correctly assign names
-names(rf_data) <- c("cancer", "adenoma")
+combined_data_hl <- sapply(scfas, 
+                  function(x) make_rf_data(x, nzv_full_pi_data, all_scfa_data, 
+                                            c(metaF$initial, metaF$followUp), "high_low"), simplify = F)
+
 
 # run through a 100 different 80/20 splits for modeling
 for(j in 1:100){
   
   # Create test and train data for both conditions
-  final_rf_data <- sapply(models, 
-                          function(x) eighty_twenty_split(x, rf_data), simplify = F)
+  final_rf_data <- sapply(scfas, 
+                          function(x) eighty_twenty_split(x, combined_data_hl), simplify = F)
   
   # Create the two models
-  rf_models <- sapply(models, 
-                      function(x) make_rf_model(x, j, "training_data", "disease", "rf", "ROC", final_rf_data),
+  rf_models <- sapply(scfas, 
+                      function(x) make_rf_model(x, j, "training_data", "high_low", "rf", "ROC", final_rf_data),
                       simplify = F)
   
   # grab the importance variables MDA
-  model_important_vars <- sapply(models, 
+  model_important_vars <- sapply(scfas, 
                                  function(x) 
-                                   grab_importance(x, j, rf_models, model_important_vars), simplify = F)
+                                   grab_importance(x, j, rf_models, class_important_vars), simplify = F)
   
   # Run the prediction
-  rf_test_data <- sapply(models, 
+  rf_test_data <- sapply(scfas, 
                          function(x) 
-                           run_prediction(x, rf_models, "test_data", "raw", "disease", final_rf_data), 
+                           run_prediction(x, rf_models, "test_data", "raw", "high_low", final_rf_data), 
                          simplify = F)
   
   # Generate the ROC curves
-  test_roc <- sapply(models, 
-                     function(x) get_test_roc(x, "disease", "tempPredictions", rf_test_data), simplify = F)
+  test_roc <- sapply(scfas, 
+                     function(x) get_test_roc(x, "high_low", "tempPredictions", rf_test_data), simplify = F)
   
   # Generate the summary data
-  model_summary_data <- sapply(models, function(x) 
-    add_model_summary_data(x, rf_models, test_roc, model_summary_data), 
+  class_summary_data <- sapply(scfas, function(x) 
+    add_model_summary_data(x, rf_models, test_roc, class_summary_data), 
     simplify = F)
   
 }
 
 # Write out the necessary data files
-sapply(models, function(x) 
-  write_csv(model_summary_data[[x]], 
+sapply(scfas, function(x) 
+  write_csv(class_summary_data[[x]], 
             paste("data/process/tables/", x, "_classification_RF_summary.csv", sep = "")))
 
-sapply(models, function(x) 
-  write_csv(model_important_vars[[x]], 
+sapply(scfas, function(x) 
+  write_csv(class_important_vars[[x]], 
             paste("data/process/tables/", x, "_imp_otus_classification_RF_summary.csv", sep = "")))
 
