a05ac84d58c9cf5320e13b377224042d014d37d7
marcsze <marcasze@gmail.com>
2017-10-31 15:12:49 -0400

    add brief discriptions of pathways in top 20

--- code/get_top_picrust_model_pathways.R
+++ code/get_top_picrust_model_pathways.R
@@ -56,9 +56,44 @@ summary_imp_lists <- sapply(c("adn_model_imp", "crc_model_imp"),
                             function(x) group_stats(x), simplify = F)
   
 top_ten_pathways <- sapply(c("adn_model_imp", "crc_model_imp"), 
-               function(x) top_model_pathways(summary_imp_lists[[x]], kegg_pathway, 10), simplify = F)
+               function(x) top_model_pathways(summary_imp_lists[[x]], kegg_pathway, 20), simplify = F)
 
 
 
+# CRC gene functions
+# gingipains R - amino acid metabolism
+# MFS Transporter - drug resistance
+# taurine-pyruvate aminotransferase - bile acid
+# 3-oxo-5-alpha-steroid 4-dehydrogenase 2  - steroid metabolism (human)
+# aldehyde dehydrogenase - acetate (aerobic bacteria)
+# PTS system (phosphotransferase system) - carbohydrate translocation and phosphorylation
+# AraC family transcriptional regulator - Transcription 
+# streptolysin S associated protein - toxin
+# phosphotransacylase - acetate
+# glycerol-1-phosphate dehydrogenase - membrane phospholipids (Archaea)
+# cystine reductase - amino acid metabolism
+# betaine reductase - amino acid metabolism
+# 1-propanol dehydrogenase - alcohol 
+# glucosyltransferase - glucose transfer
+# galactitol-1-phosphate 5-dehydrogenase - galactose metabolism
+# xanthine dehydrogenase - purine, pyrimidines, pterins, aldehydes
+# LysR family transcriptional regulator - transcription
+
+# Adn gene functions
+# phosphotransacylase - acetate
+# 1-propanol dehydrogenase - alcohol
+# adenosylcobinamide hydrolase - cofactor biosynthesis
+# fructose 1,6-bisphosphate aldolase/phosphatase - gluconeogenesis (thermophilic) (propionate)
+# 3-dehydro-L-gulonate-6-phosphate decarboxylase - ascorbate and aldarate, pentose and glucuronate interconversion
+# L-fuculokinase - fructose and mannose metabolism
+# Bax protein - Cell death (human)
+# gingipain R - amino acid metabolism
+# crotonobetainyl-CoA:carnitine CoA-transferase  - CoA transferase (butyrate)
+# DeoR family transcriptional regulator - Transcription
+# tRNA-intron endonuclease - tRNA maturation
+# anaerobic dimethyl sulfoxide reductase subunit C - anaerobic respiration
+# trimethylamine corrinoid protein - methanogenesis
+# cinnamoyl-CoA:phenyllactate CoA-transferase - CoA transferase
+# phosphoribosylaminoimidazolecarboxamide formyltransferase - purine metabolism
 
 
