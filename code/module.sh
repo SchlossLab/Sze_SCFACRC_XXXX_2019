e877033db3ed18030b35921f7bdc6ee48be3f2d0
Marc Sze <marcasze@gmail.com>
2018-01-29 15:09:54 -0500

    update with niel suggestion to use geo tool sets on flux

--- code/module.sh
+++ code/module.sh
@@ -2,6 +2,14 @@
 
 #Load needed modules
 # Needs to be executed with the source command not with bash
+
+
+# These are from Greg Dick's lab (Niel helped point these out)
+# One way to get concoct to work on flux
+module use /dept/geology/geomicro/data9/flux/modulefiles
+module load geomicro/omics
+
+# Other modules that are needed but can be loaded as normal on flux
 module load R/3.3.3
 module load sratoolkit/2.8.2-1
 module load samtools/1.3.1
@@ -10,6 +18,8 @@ module load sickle/1.33.6
 module load bedtools2/2.20.1
 module load megahit/1.0.6
 module load python-anaconda-arc-connect/latest
+
+
 
 # Control pathing
 export BOWTIE2_INDEXES=/nfs/turbo/schloss-lab/msze/active_projects/metagenome_practice/data/references
