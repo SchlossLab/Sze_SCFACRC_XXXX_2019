69b14aacd156bbf23f86cf2a815a3f25a479d5d2
Marc Sze <marcasze@gmail.com>
2018-01-30 07:40:10 -0500

    complete code to remove short contigs and write back out

--- code/remove_short_contigs.py
+++ code/remove_short_contigs.py
@@ -17,7 +17,7 @@ workdir = "data/raw/"
 refdir = "data/references/"
 all_contig = "all_contigs.fasta"
 output_file = "all_contigs_greater1kb.fasta"
-
+min_contig_length = 1000
 
 ############################################################################################
 
@@ -27,6 +27,7 @@ def get_needed_data(sampleList):
 	# Set up empty dictionaries
 	temp_dict = {}
 	count_dict = {}
+	name_dict = {}
 
 	contig_file = open("%s%s" % (workdir, all_contig), 'r')
 
@@ -40,19 +41,77 @@ def get_needed_data(sampleList):
 
 			tempName = all_info[0]
 
+			temp_full_name = line.strip('\n')
+
 		else:
 
-			sequence_info = line
+			sequence_info = line.strip('\n')
 
 			temp_dict[tempName] = sequence_info
 
 			count_dict[tempName] = temp_length
 
+			name_dict[tempName] = temp_full_name
 
-	return temp_dict, count_dict
+
+	return temp_dict, count_dict, name_dict
 
 
 # Function to only write back out to file contigs larger than 1Kb
+def remove_contigs(seqDict, countDict, nameDict, cutoff):
+
+	tempName = {}
+	tempSeq = {}
+
+	for contig in nameDict:
+
+		if int(countDict[contig]) > int(cutoff):
+
+			tempName[contig] = nameDict[contig]
+
+			tempSeq[contig] = seqDict[contig]
+
+
+	return tempName, tempSeq
+
+# Function to create a fasta with only 1Kb or above contigs
+def create_new_contig_file(nameDict, contigDict):
+
+	# Set up initial file call and directory variable to search
+	file_dirs = os.listdir(workdir)
+	file_present = False
+	# Set initial counter to 0
+	x = 0
+	# iterate through the directory list and only stop if the file end is reached
+	# or if file with the same name is found
+	while file_present == False and x <= (len(file_dirs)-1):
+
+		# check for file in directory
+		if output_file in file_dirs[x]:
+			# change to true if present
+			file_present = True
+
+		else:
+
+			file_present = False
+		# add one to the counter
+		x += 1
+	# Only create combined contig fasta if file doesn't already exist
+	if file_present == False:
+		# Sets up the file that will be read into
+		test = open("%s%s" % (workdir, output_file), 'w')
+
+		for seq_name in contigDict:
+			
+			temp_name = nameDict[seq_name]
+			temp_contig = contigDict[seq_name]
+
+			test.write(temp_name+'\n'+temp_contig+'\n')
+
+		test.close() 
+
+	else:
+		print("File already exists.") 
 
 
 
@@ -61,9 +120,10 @@ def main():
 
 	meta_genome_file_name = command_line()
 	samples_to_be_used = create_samples_to_download(meta_genome_file_name)
-	seq_dict, length_dict = get_needed_data(samples_to_be_used)
-
-	print(len(seq_dict), len(length_dict))
+	seq_dict, length_dict, fullname_dict = get_needed_data(samples_to_be_used)
+	final_name_dict, final_contig_dict = remove_contigs(seq_dict, length_dict, fullname_dict, min_contig_length)
+	create_new_contig_file(final_name_dict, final_contig_dict)
+	
 
 
 
