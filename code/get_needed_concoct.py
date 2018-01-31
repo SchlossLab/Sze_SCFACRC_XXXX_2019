6fd07be3dc2cea2cfad6d5287ddaaf5def451c27
Marc Sze <marcasze@gmail.com>
2018-01-31 13:31:56 -0500

    put down frame work for bam creation file

--- code/get_needed_concoct.py
+++ code/get_needed_concoct.py
@@ -21,8 +21,18 @@ refdir = "data/references/"
 
 ############################################################################################
 
-# create a fai file
-# convert to bam
+def create_bam_files(sampleList, contigName, outputName):
+
+	# create fai file
+	print("samtools faidx %s.fasta" % (contigName))
+	
+
+	for sam_file in sampleList:
+		# convert to bam 
+		print("samtools view -bt %s.fasta.fai %s%s_%s_bowtie.sam > %s%s_%s.bam" % 
+			(contigName, workdir, sam_file, outputName, workdir, sam_file, outputName))
+
+
 # sort and index the bam
 # mark duplicates and sort
 # get coverage data
@@ -34,7 +44,7 @@ refdir = "data/references/"
 def main(sampleListFile, contigFile, outputEnding):
 	# read in file list from -s call
 	samples_to_be_used = create_samples_to_download(sampleListFile)
-	
+	create_bam_files(samples_to_be_used, contigFile, outputEnding)
 
 
 
@@ -43,7 +53,7 @@ if __name__ == '__main__':
 	# Command line argument parser with tags for componenets within the program
 	parser = argparse.ArgumentParser(description=__doc__)
 	parser.add_argument("-l", "--sample_list", default="%swhole_metagenome_samples.txt" % (workdir), type=str, help="Text file with list of samples\n")
-	parser.add_argument("-c", "--contig_file", default="%sall_contigs.fasta" % (workdir), type=str, help="Combined contig fasta file\n")
+	parser.add_argument("-c", "--contig_file", default="%sall_contigs_1kbto10kb" % (workdir), type=str, help="Combined cut contig fasta fil name\n")
 	parser.add_argument("-s", "--sam_file_end", default="contig_1to10kb", type=str, help="Unique Ending of Samfiles\n")
 	args = parser.parse_args()
 	# Runs the main function with the following cmd line arguments ported into it
