e381b17e853ec541fd04d1588eff054e214468d5
Marc Sze <marcasze@gmail.com>
2018-02-06 10:13:18 -0500

    update code to only include adapters

--- code/qual_trim.py
+++ code/qual_trim.py
@@ -78,7 +78,7 @@ def convert_sra_to_fastq(keep_files):
 ### the SRA. 
 
 # Function to run the cutadapt step 
-def run_cutadpat(keep_files, metafileName):
+def run_cutadpat(keep_files, metafileName, runReverseComplement):
 
 	# set a counter
 	x = 0
@@ -97,44 +97,53 @@ def run_cutadpat(keep_files, metafileName):
 			# split into a list based on the \t 
 			temp_vector = temp_line.split('\t')
 			# add a full sequence primer that includes adapter and index
-			F_temp_samples[temp_vector[0]] = "%s%s%s" % (temp_vector[4], temp_vector[5], temp_vector[6])
+			F_temp_samples[temp_vector[0]] = temp_vector[3]
 
 			# temporarily store the reverse sequence primer 
-			temp_R_seq = temp_vector[7]
-			# create an empty character place holder
-			rev_seq = ""
-			# run through each chr and change to the complement base
-			for i in temp_R_seq:
+			temp_R_seq = temp_vector[4]
 
-				if i == "A":
+			if runReverseComplement == False:
 
-					rev_seq = "%sT" % (rev_seq)
+				rev_seq = temp_R_seq
 
-				elif i == "T":
+				R_temp_samples[temp_vector[0]] = rev_seq
+
+			else:
+
+				# create an empty character place holder
+				rev_seq = ""
+				# run through each chr and change to the complement base
+				for i in temp_R_seq:
+
+					if i == "A":
+
+						rev_seq = "%sT" % (rev_seq)
+
+					elif i == "T":
 				
-					rev_seq = "%sA" % (rev_seq)
+						rev_seq = "%sA" % (rev_seq)
 
-				elif i == "C":
+					elif i == "C":
 
-					rev_seq = "%sG" % (rev_seq)
+						rev_seq = "%sG" % (rev_seq)
 
-				elif i == "G":
+					elif i == "G":
 
-					rev_seq = "%sC" % (rev_seq)
+						rev_seq = "%sC" % (rev_seq)
 				
-				else:
+					else:
 
-					rev_seq = "%sN" % (rev_seq)
+						rev_seq = "%sN" % (rev_seq)
 
-			# add the reverse complement to the reverse dictionary
-			R_temp_samples[temp_vector[0]] = rev_seq[::-1]
+				# add the reverse complement to the reverse dictionary
+				R_temp_samples[temp_vector[0]] = rev_seq[::-1]
 		# Move counter up by 1
 		x += 1
 	# for each sample run the cutadapt code to remove them
 	for sample_fastq in keep_files:
 		# Runs the actual cutadapt command
 		os.system("cutadapt --error-rate=0.1 --overlap=10 \
--a %s -A %s -o %s%s_adp_trim_1.fastq -p %s%s_adp_trim_2.fastq \
+-a %s -a %s -o %s%s_adp_trim_1.fastq -p %s%s_adp_trim_2.fastq \
 %s%s_qf_1.fastq %s%s_qf_2.fastq" % 
 			(F_temp_samples[sample_fastq], R_temp_samples[sample_fastq], 
 				workdir, sample_fastq, workdir, sample_fastq, 
@@ -181,14 +190,14 @@ def run_quality_filter(keep_files, runningCutadapt):
 
 
 # Run the actual program
-def main(sampleListFile, needCutadapt, primerfileName):
+def main(sampleListFile, needCutadapt, primerfileName, needRC):
 	samples_to_download = create_samples_to_download(sampleListFile)
 	download_files(samples_to_download)
 	convert_sra_to_fastq(samples_to_download)
 	
 	if needCutadapt == True:
 
-		run_cutadpat(samples_to_download, primerfileName)
+		run_cutadpat(samples_to_download, primerfileName, needRC)
 		run_quality_filter(samples_to_download, needCutadapt)
 
 	else:
@@ -212,6 +221,11 @@ if __name__ == '__main__':
 		type=str, help="Must have the following order for columns in a \
 		tab-delimited text file: sample_name, subjectID, diseaseClass, \
 		forward adapter, forward index, forward primer, reverse primer.\n")
+	parser.add_argument("-rc", "--reverse_complement", 
+		default=False, 
+		type=bool, help="Argument to select whether to reverse complement \
+		the provided reverse adapter. The default 'False' assumes that the data \
+		provided is already reverse complemented.\n")
 	args = parser.parse_args()
 	# Runs the main function with the following cmd line arguments ported into it
-	main(args.sample_list, args.cut_adapt, args.meta_data)
+	main(args.sample_list, args.cut_adapt, args.meta_data, args.reverse_complement)
