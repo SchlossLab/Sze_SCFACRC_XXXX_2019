713934472fd45c02771432a43b20c1a1772506ef
Marc Sze <marcasze@gmail.com>
2018-02-06 08:16:50 -0500

    make code more flexible

--- code/remove_short_contigs.py
+++ code/remove_short_contigs.py
@@ -7,29 +7,26 @@
 ############## Internal parameters used by all functions is program ########################
 
 # Import needed libraries
-import os, sys, re
+import os, sys, re, argparse
 
 # Import other code with useful functions that will be used
-from qual_trim import command_line, create_samples_to_download
+from qual_trim import create_samples_to_download
 
 # Set up working directory
 workdir = "data/raw/"
 refdir = "data/references/"
-all_contig = "all_contigs.fasta"
-output_file = "all_contigs_greater1kb.fasta"
-min_contig_length = 1000
 
 ############################################################################################
 
 # Function to read in the all_contig file and return need dictionaries
-def get_needed_data(sampleList):
+def get_needed_data(sampleList, all_contig):
 
 	# Set up empty dictionaries
 	temp_dict = {}
 	count_dict = {}
 	name_dict = {}
 
-	contig_file = open("%s%s" % (workdir, all_contig), 'r')
+	contig_file = open(all_contig, 'r')
 
 	for line in contig_file:
 		
@@ -75,7 +72,7 @@ def remove_contigs(seqDict, countDict, nameDict, cutoff):
 	return tempName, tempSeq
 
 # Function to create a fasta with only 1Kb or above contigs
-def create_new_contig_file(nameDict, contigDict):
+def create_new_contig_file(nameDict, contigDict, output_file):
 
 	# Set up initial file call and directory variable to search
 	file_dirs = os.listdir(workdir)
@@ -99,7 +96,7 @@ def create_new_contig_file(nameDict, contigDict):
 	# Only create combined contig fasta if file doesn't already exist
 	if file_present == False:
 		# Sets up the file that will be read into
-		test = open("%s%s" % (workdir, output_file), 'w')
+		test = open(output_file, 'w')
 
 		for seq_name in contigDict:
 			
@@ -116,15 +113,36 @@ def create_new_contig_file(nameDict, contigDict):
 
 
 # Runs the overall program 
-def main():
+def main(sampleListFile, combinedContigFileName, min_contig_length, output_name):
 
-	meta_genome_file_name = command_line()
-	samples_to_be_used = create_samples_to_download(meta_genome_file_name)
-	seq_dict, length_dict, fullname_dict = get_needed_data(samples_to_be_used)
+	samples_to_be_used = create_samples_to_download(sampleListFile)
+	seq_dict, length_dict, fullname_dict = get_needed_data(samples_to_be_used, combinedContigFileName)
 	final_name_dict, final_contig_dict = remove_contigs(seq_dict, length_dict, fullname_dict, min_contig_length)
-	create_new_contig_file(final_name_dict, final_contig_dict)
+	create_new_contig_file(final_name_dict, final_contig_dict, output_name)
 	
 
 
+# Initializes at the start of the program
+if __name__ == '__main__': 
 
-if __name__ == '__main__': main()
+	# Command line argument parser with tags for componenets within the program
+	parser = argparse.ArgumentParser(description=__doc__)
+	parser.add_argument("-s", "--sample_list", 
+		default="%swhole_metagenome_samples.txt" % (workdir), 
+		type=str, help="Text file with list of samples to run through the program.\n")
+	parser.add_argument("-cf", "--contig_file", 
+		default="%sall_contigs.fasta" % (workdir), type=str, 
+		help="The combined contig file to be parsed. \
+		The default is 'all_contigs.fasta'.\n")
+	parser.add_argument("-mc", "--min_contig", 
+		default=1000, type=int, 
+		help="The minimum size a contig needs to be to be kept. \
+		The default is 1000 bp.\n")
+	parser.add_argument("-o", "--output", 
+		default="%sall_contigs_greater1kb.fasta" % (workdir), type=str, 
+		help="File to save the remaining contigs to. \
+		The default is 'all_contigs_greater1kb.fasta'.\n")
+	args = parser.parse_args()
+
+	# Runs the main function with the following cmd line arguments ported into it
+	main(args.sample_list, args.contig_file, args.min_contig, args.output)
