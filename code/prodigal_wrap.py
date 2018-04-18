af0e01156b8c5143c947c739953b29984e47e443
Marc Sze <marcasze@gmail.com>
2018-04-18 07:38:19 -0400

    added prodigal and mmseqs2 functionality

--- code/prodigal_wrap.py
+++ code/prodigal_wrap.py
@@ -13,6 +13,9 @@ import os, sys, argparse
 # Set up working directory
 workdir = "data/raw/"
 
+# Set up refrence directory
+refdir = "data/references/"
+
 
 ############################################################################################
 
@@ -53,21 +56,105 @@ def run_prodigal(temp_folder_name):
 
 	# Combine all files together and move to prodigal run directory
 
-	os.system("%s/*.out > %s/prodigal_run/tmp_genes.fasta" % (temp_folder_name, workdir))
+	os.system("cat %s/*.out > %s/prodigal_run/tmp_genes.fasta" % (temp_folder_name, workdir))
 
-	os.system("%s/*.nucl > %s/prodigal_run/tmp_genes.nucleotide" % 
+	os.system("cat %s/*.nucl > %s/prodigal_run/tmp_genes.nucleotide" % 
 		(temp_folder_name, workdir))
+
+
+# Function to run mmseqs2 to establish the OPFs
+def run_mmseqs2(mmseq2_dir_path, query_fasta):
+
+	os.system("PATH=%s/bin:$PATH" % (mmseq2_dir_path))
+	# Create mmseq2 directory
+	if os.path.exists("%smmseq2_opf_run" % (workdir)):
+
+		print("mmseq2_opf_run directory exists")
+
+	else:
+
+		print("No mmseq2_opf_run directory found, creating...")
+
+		os.system("mkdir %smmseq2_opf_run" % (workdir))
+
+	# Create a tmp directory within the mmseq2 directory
+	if os.path.exists("%smmseq2_opf_run/tmp" % (workdir)):
+
+		print("tmp directory in mmseq2_opf_run directory exists")
+
+	else:
+
+		print("No tmp directory found in mmseq2_opf_run, creating...")
+
+		os.system("mkdir %smmseq2_opf_run/tmp" % (workdir))
+
+# 	# Set up query DB
+# 	os.system("mmseqs createdb %s %smmseq2_opf_run/query_DB" % (query_fasta, workdir))
+
+# 	# Set up target DB
+# 	os.system("mmseqs createdb %sprodigal_run/tmp_genes.fasta \
+# %smmseq2_opf_run/target_DB" % (workdir, workdir))
+
+# 	# Create index 
+
+# 	os.system("mmseqs createindex %smmseq2_opf_run/target_DB %smmseq2_opf_run/tmp" % 
+# 		(workdir, workdir))
+
+# 	# Create alignment
+
+# 	os.system("mmseqs search %smmseq2_opf_run/query_DB %smmseq2_opf_run/target_DB \
+# %smmseq2_opf_run/resultDB %smmseq2_opf_run/tmp" % (workdir, workdir, workdir, workdir))
+
+	
+	# Cluster the target DB
+	os.system("mmseqs cluster %s/mmseq2_opf_run/target_DB %s/mmseq2_opf_run/target_clu \
+%s/mmseq2_opf_run/tmp -e 0.001 --min-seq-id 0.4" % (workdir, workdir))
+
+	# Cluster the result DB
+	os.system("mmseqs cluster %s/mmseq2_opf_run/resultDB %s/mmseq2_opf_run/result_clu \
+%s/mmseq2_opf_run/tmp -e 0.001 --min-seq-id 0.4" % (workdir, workdir))
+
+	# Create a target clustered sequence file
+	os.system("mmseqs createseqfiledb %s/mmseq2_opf_run/target_DB \
+%s/mmseq2_opf_run/target_clu %s/mmseq2_opf_run/clu_seq" % (workdir, workdir))
+
+	# Create a result clustered sequence file
+	os.system("mmseqs createseqfiledb %s/mmseq2_opf_run/resultDB \
+%s/mmseq2_opf_run/result_clu %s/mmseq2_opf_run/clu_result_seq" % (workdir, workdir))
+
+	# Create target clustered fasta file
+	os.system("mmseqs result2flat %s/mmseq2_opf_run/target_DB %s/mmseq2_opf_run/target_DB \
+%s/mmseq2_opf_run/clu_seq %s/mmseq2_opf_run/clu_seq.fasta" % 
+		(workdir, workdir, workdir, workdir))
+
+	# Create a result clustered fasta file
+	os.system("mmseqs result2flat %s/mmseq2_opf_run/resultDB %s/mmseq2_opf_run/resultDB \
+%s/mmseq2_opf_run/clu_result_seq %s/mmseq2_opf_run/clu_result_seq.fasta" % 
+		(workdir, workdir, workdir, workdir))
+
+	# Create a clustered target sequence tsv file
+	os.system("mmseqs createtsv %s/mmseq2_opf_run/target_DB %s/mmseq2_opf_run/target_DB clu \
+%s/mmseq2_opf_run/clu.tsv" % 
+		(workdir, workdir, workdir))
+
+	# Create a clustered result sequence tsv file
+	os.system("mmseqs createtsv %s/mmseq2_opf_run/resultDB %s/mmseq2_opf_run/resultDB clu \
+%s/mmseq2_opf_run/clu_result.tsv" % 
+		(workdir, workdir, workdir))
+
 
 
 
 
 
 # Run the actual program
-def main(contig_fasta, FileSize, splitLines, temp_folder):
+def main(contig_fasta, FileSize, splitLines, temp_folder, mmseq2_dir, query_DB_fasta):
 	
-	split_files(contig_fasta, FileSize, splitLines, temp_folder)
+	#split_files(contig_fasta, FileSize, splitLines, temp_folder)
 
-	run_prodigal(temp_folder)
+	#run_prodigal(temp_folder)
+
+	run_mmseqs2(mmseq2_dir, query_DB_fasta)
 	
 	
 # Initialized when program starts
@@ -87,7 +174,18 @@ if __name__ == '__main__':
 	parser.add_argument("-td", "--temp_dir", default="%stmp" % (workdir), 
 		type=str, help="Temporary directory in which split files will go. \
 		Prodigal analysis will be completed within this temp dir. \
-		Specify with the '-td' tag with the defaul set to 'tmp'.\n")
+		Specify with the '-td' tag with the default set to 'tmp'.\n")
+	parser.add_argument("-mmd", "--mmseqs2_dir", default="/home/marcsze/bin/MMseqs2", 
+		type=str, help="Directoy on system the mmseqs2 program is stored in. \
+		This will be used to set the path in linux based systems for the program call. \
+		Specify with the '-mmd' tag with the default set to '/home/marcsze/bin/MMseqs2'.\n")
+	parser.add_argument("-qd", "--query_database", 
+		default="%sgenes.pep.format.fasta" % (refdir), 
+		type=str, help="Reference data base used during OPF mmseq2 search. \
+		This is used to specify the query fasta to make the query DB from. \
+		Specify with the '-qd' tag with the default set to 'genes.pep.format.fasta' \
+		from the KEGG 2016 genes database.\n")
 	args = parser.parse_args()
 	# Runs the main function with the following cmd line arguments ported into it
-	main(args.contig_file, args.max_size, args.split_size, args.temp_dir)
+	main(args.contig_file, args.max_size, args.split_size, args.temp_dir, 
+		args.mmseqs2_dir, args.query_database)
