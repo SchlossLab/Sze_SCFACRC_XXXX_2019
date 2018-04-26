4f1c31c1534f0e8c31b285f2092ff2b8ed30d39e
Marc Sze <marcasze@gmail.com>
2018-04-26 19:41:09 -0400

    fix error with removal of bam files

--- code/get_needed_concoct.py
+++ code/get_needed_concoct.py
@@ -152,12 +152,12 @@ def get_cover_and_link(sampleList, contigName, outputName, proportion_to_sample)
 %soverall_coverage_table.tsv" % 
 (workdir, contigName, workdir, samples_to_keep, workdir))
 
-	# Remove the unneeded bam files
+	# Remove the uneeded bam files
 	for rm_files in sampleList:
 
 		if rm_files not in samples_to_keep:
 
-			os.system("rm %s%s_%s_sort_rmdup_sort.bam" % (workdir, rm_files, contigName))
+			os.system("rm %s%s_contig_1to10kb_sort_rmdup_sort.bam" % (workdir, rm_files))
 
 
 	# creates the linkage table
@@ -180,8 +180,8 @@ def get_cover_and_link(sampleList, contigName, outputName, proportion_to_sample)
 def main(sampleListFile, contigFile, outputEnding, sample_proportion):
 	# read in file list from -s call
 	samples_to_be_used = create_samples_to_download(sampleListFile)
-	create_bam_files(samples_to_be_used, contigFile, outputEnding)
-	mark_duplicates(samples_to_be_used, outputEnding)
+	# create_bam_files(samples_to_be_used, contigFile, outputEnding)
+	# mark_duplicates(samples_to_be_used, outputEnding)
 	get_cover_and_link(samples_to_be_used, contigFile, outputEnding, sample_proportion)
 
 
