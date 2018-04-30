a1c460b3779d40f650b0d1bf8e74b8fc4c722a97
marcsze <marcasze@gmail.com>
2018-04-30 14:50:49 -0400

    update with newest OPF data

--- code/run_303030_subset_analysis.R
+++ code/run_303030_subset_analysis.R
@@ -3,27 +3,55 @@
 ### Marc Sze
 
 
-
 # Load in needed functions and libraries
 source('code/functions.R')
 
 # Load needed libraries
-loadLibs(c("tidyverse"))
+loadLibs(c("tidyverse", "caret"))
 
 # Load in needed data
-meta_data <- read_tsv("data/raw/metadata/303030_subset/NexteraXT003Map.tsv", col_names = F) %>% 
-  select(X2, X22, X28, X30) %>% 
-  rename(hannigan_name = X2, sample_id = X22, sample_type = X28, disease = X30)
+meta_data <- read_tsv("data/process/sra_meta_conversion.txt") %>% 
+  filter(samp_mat_process != "Virome") %>% 
+  select(Run, Sample_Name, DiseaseClass) %>% 
+  rename(sample_id = Run, hannigan_name = Sample_Name, disease = DiseaseClass)
+  
+opf_data <- read_tsv("data/process/opf_shared.tsv")
 
-opf_data <- read_tsv("data/process/ClusteredOpfAbund.tsv") %>% 
-  rename(gene_name = V1, hannigan_name = V2, cor_counts = sum)
+# Get clusters that are not singletons
+greater_than_1_count <- opf_data %>% 
+  group_by(opf_cluster) %>% 
+  summarise(total_counts = sum(cor_total_counts)) %>% 
+  filter(total_counts > 1) %>% 
+  pull(opf_cluster)
 
+# Remove these from the data file
+opf_data <- opf_data %>% 
+  filter(opf_cluster %in% greater_than_1_count)
 
+# Get counts of clusters that are due to more than 1 sample 
+in_more_than_1_individual <- opf_data %>% 
+  group_by(opf_cluster) %>% 
+  summarise(sample_counts = n_distinct(sample_id)) %>% 
+  filter(sample_counts > 1) %>% 
+  pull(opf_cluster)
+  
+# Remove these from the data file
+opf_data <- opf_data %>% 
+  filter(opf_cluster %in% in_more_than_1_individual)
 
-
-
-
-
+# Spread the data to make it amenable for near zero variance testing
+test <- opf_data %>% 
+  select(-sum_counts, -total_opf_members) %>% 
+  spread(opf_cluster, cor_total_counts, fill = 0) %>% 
+  left_join(select(meta_data, sample_id, disease), by = "sample_id") %>% 
+  select(sample_id, disease, everything())
+ 
+# Generate the near zero variance list 
+nzv <- nearZeroVar(as.data.frame(select(test, -sample_id)))
+# Generate the final data frame holder
+filtered_df <- as.data.frame(select(test, -sample_id))
+# Filter out the genes with near zero variance from the final data frame
+filtered_df <- filtered_df[, -nzv]
 
 
 
