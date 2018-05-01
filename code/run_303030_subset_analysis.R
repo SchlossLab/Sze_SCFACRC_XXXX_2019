951174f7e5d278c38faeaea990549a7634afe041
marcsze <marcasze@gmail.com>
2018-05-01 14:50:05 -0400

    add dunn and kruskal complete test and write out

--- code/run_303030_subset_analysis.R
+++ code/run_303030_subset_analysis.R
@@ -7,7 +7,7 @@
 source('code/functions.R')
 
 # Load needed libraries
-loadLibs(c("tidyverse", "caret"))
+loadLibs(c("tidyverse", "caret", "FSA"))
 
 # Load in needed data
 meta_data <- read_tsv("data/process/sra_meta_conversion.txt") %>% 
@@ -68,7 +68,7 @@ filtered_df <- filtered_df %>%
 rm(greater_than_1_count, in_more_than_1_individual, test, opf_data)
 
 # Run a kruskal test to find differences between specific genes
-test <- filtered_df %>% 
+kruskal_test <- filtered_df %>% 
   mutate(disease = factor(disease, 
                           levels = c("Healthy", "Adenoma", "Cancer"), 
                           labels = c("Normal", "Adenoma", "Cancer"))) %>% 
@@ -81,6 +81,80 @@ test <- filtered_df %>%
   select(opf_cluster, method, parameter, statistic, p.value) %>% 
   mutate(bh = p.adjust(p.value, method = "BH")) %>% 
   arrange(p.value)
+
+# Run a dunn's test post-hoc to find which components are most significant 
+# Used a P-value cutoff of 0.05 since nothing was significant after multiple comparison correction
+opf_clusters_to_keep <- kruskal_test %>% 
+  filter(p.value < 0.05) %>% 
+  pull(opf_cluster)
+
+dunn_test <- filtered_df %>% 
+  mutate(disease = factor(disease, 
+                          levels = c("Healthy", "Adenoma", "Cancer"), 
+                          labels = c("Normal", "Adenoma", "Cancer"))) %>% 
+  filter(opf_cluster %in% opf_clusters_to_keep) %>% 
+  group_by(opf_cluster) %>% 
+  nest() %>% 
+  mutate(dunn_analysis = map(data, ~dunnTest(corr_counts ~ disease, data = .x, method = "bh")[["res"]])) 
+
+dunn_testing <- dunn_test %>% select(opf_cluster, dunn_analysis) %>% unnest()
+
+# Find all groups that have a consistent direction
+all_neg <- dunn_testing %>% group_by(opf_cluster) %>% 
+  count(fulfills_req = Z < 0) %>% 
+  filter(n == 3 & fulfills_req == TRUE) %>% 
+  pull(opf_cluster)
+
+all_pos <- dunn_testing %>% group_by(opf_cluster) %>% 
+  count(fulfills_req = Z > 0) %>% 
+  filter(n == 3 & fulfills_req == TRUE) %>% 
+  pull(opf_cluster)
+  
+direction_consistent_dunn <- dunn_testing %>% 
+  filter(opf_cluster %in% c(all_neg, all_pos))
+
+# Pull only those that have at least 2 out of 3 significant comparisons
+sig_comparisons <- direction_consistent_dunn %>% 
+  group_by(opf_cluster) %>% 
+  count(fulfills_req = P.adj < 0.05) %>% 
+  filter(n >= 2 & fulfills_req == TRUE) %>% 
+  pull(opf_cluster)
+
+sig_dir_consistent_dunn <- direction_consistent_dunn %>% 
+  filter(opf_cluster %in% sig_comparisons)
+
+# Find only those that have carcinoma and adenoma different versus normal
+specific_sig_comparisons <- sig_dir_consistent_dunn %>% 
+  group_by(opf_cluster) %>% 
+  filter(Comparison == "Adenoma - Normal" | Comparison == "Cancer - Normal") %>% 
+  count(fulfills_req = P.adj < 0.05) %>% 
+  filter(n == 2 & fulfills_req == TRUE) %>% 
+  pull(opf_cluster)
+
+specific_sig_dir_cons_dunn <- sig_dir_consistent_dunn %>% 
+  filter(opf_cluster %in% specific_sig_comparisons)
+
+# Arrange by most significant groups
+most_sig_compare <- specific_sig_dir_cons_dunn %>% 
+  filter(Comparison != "Adenoma - Cancer") %>% 
+  group_by(opf_cluster) %>% 
+  summarise(most_sig_mean = mean(P.adj)) %>% 
+  arrange(most_sig_mean) %>% 
+  pull(opf_cluster)
+
+ordered_sp_sig_dir_cons_dunn <- specific_sig_dir_cons_dunn %>% 
+  mutate(opf_cluster = factor(opf_cluster, 
+                              levels = most_sig_compare, 
+                              labels = most_sig_compare)) %>% 
+  arrange(opf_cluster)
+
+
+# Write out the data frames of interest
+write_csv(kruskal_test, "data/process/opf_kruskal_analysis.csv")
+
+write_csv(ordered_sp_sig_dir_cons_dunn, "data/process/opf_dunn_select_results.csv")
+
+
 
 
 
