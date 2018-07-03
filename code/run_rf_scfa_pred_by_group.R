1b0ffaf8ed35a007137b7d6c2e4a44c1551fd09e
marcsze <marcasze@gmail.com>
2018-07-03 10:15:32 -0400

    first pass model roll out

--- code/run_rf_scfa_pred_by_group.R
+++ code/run_rf_scfa_pred_by_group.R
@@ -1,73 +1,3 @@
-### Investigate how important OTUs based on MDA change for each group
-### Using RF models do OTUs change if in non-tumor, adenoma, or carcinoma samples
-### Marc Sze
-
-# Load in needed functions and libraries
-source('code/functions.R')
-
-# Load needed libraries
-loadLibs(c("tidyverse", "caret", "pROC", "randomForest"))
-
-#setup variables that will be used
-scfas <- c("acetate", "butyrate", "isobutyrate", "propionate")
-
-# Samples that had to little volume to actually be measured
-not_measured <- c("3367653", "2041650", "2043650", "3027650")
-
-
-##############################################################################################
-############### List of functions to get things to run nice ##################################
-##############################################################################################
-
-# Function to load in the respective scfa data
-upload_scfa_data <- function(scfa_name, path_to_file, ending){
-  # scfa_name is a variable that stores the scfa of interest e.g. "acetate"
-  # path_to_file stores the directory path where the files are
-  # ending is the common ending of the file of interest
-  
-  # the command that reads in the files
-  tempData <- read_csv(paste(path_to_file, scfa_name, ending, sep = "")) %>% 
-    rename(Group = study_id)
-  # sends the data to the global work environment
-  return(tempData)
-}
-
-# Function to remove specific data from sets of interest
-pare_down_data <- function(ids_to_remove, scfa_list, otu_data, meta_data){
-  
-  # Remove data from tables first
-  temp_otuData <- otu_data %>% filter(!(Group %in% ids_to_remove))
-  
-  nzv <- nearZeroVar(temp_otuData)
-  
-  nzv_rm_tempOTU <- temp_otuData[, -nzv]
-  
-  temp_meta_data <- meta_data %>% filter(!(Group %in% ids_to_remove))
-  
-  # Remove data from lists
-  tempList <- lapply(scfa_list, 
-                     function(x) filter(x, !(Group %in% ids_to_remove)) %>% 
-                       inner_join(temp_meta_data, by = "Group") %>% 
-                       inner_join(nzv_rm_tempOTU, by = "Group")) 
-  
-  return(tempList)
-}
-
-# Function to generate the high low groupings for each respective SCFA
-get_high_low <- function(all_data_list, scfa_median_list, scfas){
-  
-  tempList <- sapply(scfas, 
-                     function(x) all_data_list[[x]] %>% 
-                       mutate(high_low = ifelse(mmol_kg <= scfa_median_list[[x]], 
-                                                invisible("low"), invisible("high"))) %>% 
-                       select(Group, mmol_kg, high_low, Dx_Bin, dx, fit_result, everything()), 
-                     simplify = F)
-  
-  return(tempList)
-  
-}
-
-
 
 ##############################################################################################
 ############### Run the actual programs to get the data ######################################
@@ -85,10 +15,6 @@ shared <- read_tsv("data/process/final.0.03.subsample.shared") %>%
   mutate(Group = as.character(Group)) %>% 
   select(-label, -numOtus) 
 
-log_shared <- read_tsv("data/process/final.0.03.subsample.shared") %>% 
-  mutate(Group = as.character(Group)) %>% 
-  select(-label, -numOtus) %>% 
-  mutate_at(vars(contains("Otu")), funs(log10(. + 1)))
 
 # Generate IDs that are initial and follow up
 initial_IDs <- as.character(metaF$initial)
@@ -101,3 +27,79 @@ scfa_data <- sapply(scfas,
 
 # Combine all data into a single list of lists and remove follow samples
 all_data <- pare_down_data(c(initial_IDs, follow_IDs), scfa_data, shared, picrust_meta)
+
+
+# Get medians for each scfa measured
+scfa_medians <- lapply(all_data, function(x) median(x$mmol_kg))
+
+# get final data tables to be used in the RF analysis
+final_data <- get_high_low(all_data, scfa_medians, scfas)
+
+
+# Split into normal, adenoma, and cancer groups
+split_final_data <- list(
+  final_data_normal = sapply(scfas, 
+                             function(x) final_data[[x]] %>% 
+                               filter(dx == "normal"), simplify = F), 
+  final_data_adenoma = sapply(scfas, 
+                              function(x) final_data[[x]] %>% 
+                                filter(dx == "adenoma"), simplify = F), 
+  final_data_carcinoma = sapply(scfas, 
+                                function(x) final_data[[x]] %>% 
+                                  filter(dx == "cancer"), simplify = F))
+
+# Set up initial store for summary data
+class_summary_model_data <- list(
+  final_data_normal = list(acetate = data_frame(), butyrate = data_frame(), 
+                           isobutyrate = data_frame(), propionate = data_frame()), 
+  final_data_adenoma = list(acetate = data_frame(), butyrate = data_frame(), 
+                            isobutyrate = data_frame(), propionate = data_frame()), 
+    final_data_carcinoma = list(acetate = data_frame(), butyrate = data_frame(), 
+                              isobutyrate = data_frame(), propionate = data_frame()))
+  
+  
+class_important_vars <- list(
+  final_data_normal = list(acetate = data_frame(), butyrate = data_frame(), 
+                           isobutyrate = data_frame(), propionate = data_frame()), 
+  final_data_adenoma = list(acetate = data_frame(), butyrate = data_frame(), 
+                            isobutyrate = data_frame(), propionate = data_frame()), 
+  final_data_carcinoma = list(acetate = data_frame(), butyrate = data_frame(), 
+                              isobutyrate = data_frame(), propionate = data_frame()))
+
+
+# Run a 100 different splits for each group and SCFA on classification
+for(i in names(split_final_data)){
+  
+  print(paste("Getting model data from ", i, " only...."))
+  
+  for(j in 1:1){
+    
+    # Generate an 80/20 data split
+    rf_data <- sapply(scfas, function(x) eighty_twenty_split(x, "rf_groups", split_final_data[[i]]), simplify = F)
+    
+    class_test <- sapply(scfas, function(x)
+      make_rf_model(x, j, "training_data", "high_low", "rf", "ROC", rf_data),
+      simplify = F)
+
+    class_pred <- sapply(scfas, function(x)
+      run_prediction(x, class_test, "test_data", "raw", "high_low", rf_data),
+      simplify = F)
+
+    ROC_data <- sapply(scfas, function(x)
+      get_test_roc(x, "high_low", "tempPredictions", class_pred, classif = T), simplify = F)
+
+    class_summary_model_data <- sapply(scfas, function(x)
+      add_model_summary_data(x, class_test, ROC_data, class_summary_model_data[[i]], classif = T), simplify = F)
+
+    class_important_vars <- sapply(scfas, function(x)
+      grab_importance(x, j, "classification", class_test, class_important_vars[[i]]), simplify = F)
+  }
+  
+  print(paste("Completed getting all model data for ", i, " only."))
+}
+
+
+
+
+
+
