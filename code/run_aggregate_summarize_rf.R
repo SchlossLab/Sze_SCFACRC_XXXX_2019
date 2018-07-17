ae5189293564362ad51fce8a11ebc572600959c9
marcsze <marcasze@gmail.com>
2018-07-17 10:06:51 -0400

    add dunn functionality to workflow

--- code/run_aggregate_summarize_rf.R
+++ code/run_aggregate_summarize_rf.R
@@ -65,6 +65,31 @@ create_count_table <- function(datatable, train_data = T){
   
 }
 
+# Create needed data for the analysis
+get_reg_diff <- function(datatable, train_data = T){
+  
+  if(train_data == T){
+    
+    tempData <- datatable %>% 
+      mutate(difference = obs - pred)
+    
+  } else{
+    
+    tempData <- datatable %>% 
+      mutate(difference = mmol_kg - tempPredictions)
+  }
+  
+  tempData <- tempData %>% 
+    group_by(run, dx) %>% 
+    summarise(median_diff = median(difference), 
+              min_dff = min(difference), 
+              max_diff = max(difference)) %>% 
+    ungroup()
+  
+  return(tempData)
+}
+
+
 # Set up the names of the list
 data_sets_names <- names(model_data)
 
@@ -74,7 +99,7 @@ test_results <- list(
   log_reg_train = c(), log_reg_test = c())
 
 
-test <- model_data$reg_train$acetate %>% 
+test <- model_data$reg_test$acetate %>% 
   mutate(difference = pred - obs) %>% 
   group_by(run, dx) %>% 
   summarise(median_diff = median(difference), 
@@ -106,10 +131,44 @@ for(i in data_sets_names){
     model_type = F
   }
   
-  temp_tables <- map(model_data[[i]], function(x) create_count_table(x, train_data = model_type))
+  if(str_detect(i, "reg")){
+    
+    temp_tables <- map(model_data[[i]], function(x) get_reg_diff(x, train_data = model_type))
+    
+    analysis_summary <- map(temp_tables, function(x)
+      x %>% nest() %>%
+        mutate(k_test = map(data, ~kruskal.test(median_diff ~ factor(dx), data = .x)),
+               k_summary = map(k_test, broom::tidy)) %>%
+        select(k_summary) %>%
+        unnest())
+    
+    for(j in names(analysis_summary)){
+      
+      temp_pvalue <- analysis_summary[[j]] %>% 
+        select(p.value) %>% pull()
+      
+      str(temp_pvalue)
+      
+      if(temp_pvalue < 0.05){
+        
+        analysis_summary[[j]] <- as.data.frame.list(
+          dunn.test::dunn.test(temp_tables[[j]]$median_diff, factor(temp_tables[[j]]$dx), method = "bh"))
+      }
+    }
+      
+    test_results[[i]] <- analysis_summary
+
+      
+  } else{
+    
+    temp_tables <- map(model_data[[i]], function(x) create_count_table(x, train_data = model_type))
+    
+    test_results[[i]] <- map(temp_tables, function(x) c(statistic = unname(chisq.test(x)$statistic), 
+                                                        pvalue = chisq.test((x))$p.value))
+    
+  }
   
-  test_results[[i]] <- map(temp_tables, function(x) c(statistic = unname(chisq.test(x)$statistic), 
-                                               pvalue = chisq.test((x))$p.value))
+  
   
   print(paste("Finished analysis of ", i, "...", sep = ""))
 }
