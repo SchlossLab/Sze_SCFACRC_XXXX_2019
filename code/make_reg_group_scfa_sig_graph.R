0d5a98c2053a5972dc43b486f13ea8a84f286058
marcsze <marcasze@gmail.com>
2018-07-20 14:43:33 -0400

    update coding for good version

--- code/make_reg_group_scfa_sig_graph.R
+++ code/make_reg_group_scfa_sig_graph.R
@@ -9,26 +9,54 @@ source('code/functions.R')
 # Load needed libraries
 loadLibs(c("tidyverse", "gridExtra", "viridis"))
 
+taxonomy <- read_tsv("data/process/final.taxonomy") %>% 
+  select(-Size) %>% 
+  mutate(Taxonomy = str_replace_all(Taxonomy, "\\(\\d*\\)", "")) %>% 
+  separate(Taxonomy, c("kingdom", "phyla", "class", "order", "family", "genus", "species"), sep = ";")
 
 reg_data <- read_csv("data/process/tables/significant_reg_otu_comp_summary.csv") %>% 
-  mutate(genus = str_replace_all(genus, "_unclassified", ""), 
-         genus = str_replace_all(genus, "_", " "))
+  left_join(select(taxonomy, OTU, family), by = c("otu" = "OTU")) %>% 
+  mutate(family = str_replace_all(family, "_unclassified", ""), 
+         family = str_replace_all(family, "_", " "), 
+         direction = case_when(
+           estimate < 0 ~ "neg", 
+           estimate > 0 ~ "pos", 
+           TRUE ~ "uh oh"))
+  
 
 
 graph_data <- reg_data %>% 
-  group_by(scfa, dx, genus) %>% 
+  mutate(family = case_when(
+    family %in% c("Acidaminococcaceae", "Actinomycetaceae", "Bacteria", 
+                 "Pseudomonadaceae", "Streptococcaceae", "Sutterellaceae", 
+                 "Proteobacteria", "Firmicutes", "Bacteroidaceae", 
+                 "Clostridia", "Erysipelotrichaceae", "Oxalobacteraceae", 
+                 "Pasteurellaceae") ~ "Other", 
+    TRUE ~ family)) %>% 
+  group_by(scfa, dx, direction, family) %>% 
   count()
 
 graph_data %>% 
   ungroup() %>% 
   mutate(dx = factor(dx, 
                      levels = c("normal", "adenoma", "cancer"), 
-                     labels = c("Normal", "Adenoma", "Carcinoma"))) %>% 
-  ggplot(aes(dx, n, group = genus, fill = genus)) + 
-  geom_bar(stat = "identity") + 
+                     labels = c("Normal", "Adenoma", "Carcinoma")), 
+         family = factor(family, 
+                         levels = c('Clostridiales', 'Lachnospiraceae', 'Porphyromonadaceae', 
+                                    'Rikenellaceae', 'Ruminococcaceae', 'Other'), 
+                         labels = c('Clostridiales', 'Lachnospiraceae', 'Porphyromonadaceae', 
+                                    'Rikenellaceae', 'Ruminococcaceae', 'Other')), 
+         direction = factor(direction, 
+                            levels = c("neg", "pos"), 
+                            labels = c("Negative Correlation", "Positive Correlation")), 
+         scfa = factor(scfa, 
+                       levels = c("acetate", "butyrate", "propionate"), 
+                       labels = c("Acetate", "Butyrate", "Propionate"))) %>% 
+  ggplot(aes(dx, n, fill = family)) + 
+  geom_bar(stat = "identity") + theme_bw() + 
  # geom_bar(stat = "identity", position = position_dodge(1), show.legend = F) + 
-  facet_grid(~scfa) + 
+  facet_grid(scfa~direction) +
   labs(x = "", y = "Number of Significant OTUs") + 
-  scale_fill_viridis(discrete = T, option = "magma") + 
+  scale_fill_manual(name = "", values = c('#9B30FF', '#63B8FF', '#008080', '#8FBC8F', '#FF7F00', '#6C7B8B')) + 
   theme(legend.position = "bottom")
 
