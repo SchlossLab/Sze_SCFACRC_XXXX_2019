55ae7abf1969037287912bedd02be7a67b207621
pschloss <pschloss@umich.edu>
2019-02-07 08:48:48 -0500

    Concatenate random forest output files

--- code/rf_pipeline/cat_files.sh
+++ code/rf_pipeline/cat_files.sh
@@ -1,18 +1,11 @@
 #!/bin/bash
-SEARCH_DIR=data/temp
-FINAL_DIR=data/process
+SEED_DIR=$1
+
 # Keep the first line of File1 and remove the first line of all the others and combine
 # This script will combine cv(all) and testing(best) results
 
-for model in "Random_Forest"
-do
-  	head -1 $SEARCH_DIR/all_hp_results_"$model"_1.csv  > $SEARCH_DIR/combined_all_hp_results_"$model".csv; tail -n +2 -$
+echo "seed,mtry,ROC,Sens,Spec,ROCSD,SensSD,SpecSD,model" > $SEED_DIR/mtry_results.csv
+grep "Random_Forest" $SEED_DIR/all_hp_results_Random_Forest_*.csv | sed -E 's/.*_Random_Forest_(.*).csv:/\1,/' | sort -k 1,2 -n >> $SEED_DIR/mtry_results.csv
 
-    head -1 $SEARCH_DIR/best_hp_results_"$model"_1.csv  > $SEARCH_DIR/combined_best_hp_results_"$model".csv; tail -n +2$
-
-    mv $SEARCH_DIR/combined_all_hp_results_"$model".csv $FINAL_DIR/combined_all_hp_results_"$model".csv
-    mv $SEARCH_DIR/combined_best_hp_results_"$model".csv $FINAL_DIR/combined_best_hp_results_"$model".csv
-done
-
-rm $SEARCH_DIR/all_hp_*
-rm $SEARCH_DIR/best_hp_*
+echo "seed,cv_aucs,test_aucs,model" > $SEED_DIR/best_results.csv
+grep "Random_Forest" $SEED_DIR/best_hp_results_Random_Forest_*.csv | sed -E 's/.*_Random_Forest_(.*).csv:/\1,/' | sort -k 1,2 -n >> $SEED_DIR/best_results.csv
